51a53f4424c5a0edb2fe7992df97ddb95142e219
天道寺Tendouji <Tendouji>
2017-03-17 21:18:08 +0800

    commit #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+	shipList = [5, 9, 10, 11, 15];
+    swapiDomain = 'http://swapi.co/api/';
+    swapiUrl = this.swapiDomain + 'starships/';
+    shipAttributes = ['name', 'model', 'manufacturer', 'crew', 'edited'];
+    shipsInfo = [];
+
+    messageList = {
+        'start': 'Getting content from ' + this.swapiDomain + '...', 
+        'noInternet': 'You seemed to be offline! Please check your Internet connection.'
+    };
+
+    /* dom element */
+    preloader;
+    menuSelect;
+    messagePanel;
+    spaceshipList;
 }
